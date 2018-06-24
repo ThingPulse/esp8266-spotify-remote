cda3b2ef4e6377a47ccee3fe5a57f502e3ef4c64
Daniel Eichhorn <dani.eichhorn@squix.ch>
2018-06-24 07:34:38 +0200

    Fix initial state of isDownloadingCover

--- esp8266-spotify-remote.ino
+++ esp8266-spotify-remote.ino
@@ -1,17 +1,13 @@
 /**The MIT License (MIT)
-
-Copyright (c) 2018 by ThingPulse Ltd., https://thingpulse.com
-
+Copyright (c) 2018 by ThingPulse GmbH
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
 in the Software without restriction, including without limitation the rights
 to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
-
 The above copyright notice and this permission notice shall be included in all
 copies or substantial portions of the Software.
-
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,6 +15,7 @@ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
+See more at https://thingpulse.com
 */
 
 #include <ESP8266WiFi.h>
@@ -57,7 +54,7 @@ uint16_t counter = 0;
 long lastUpdate = 0;
 TS_Point lastTouchPoint;
 uint32_t lastTouchMillis = 0;
-boolean isDownloadingCover = true;
+boolean isDownloadingCover = false;
 
 void drawJPEGFromSpiffs(String filename, MiniGrafx *gfx, int xpos, int ypos, DrawingCallback *drawingCallback);
 void calibrationCallback(int16_t x, int16_t y);
